5214a9df93f19b6dfa0082e44197623f887713ef
Tommy Chen <mailcdm@gmail.com>
2015-05-05 18:08:35 +0800

    初始化工程

---
+++ EasyAppCore/EasyAppCore/EasyAppCore.h
@@ -0,0 +1,13 @@
+//
+//  EasyAppCore.h
+//  EasyAppCore
+//
+//  Created by cdm on 15/5/1.
+//  Copyright (c) 2015年 MyIdeaWay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EasyAppCore : NSObject
+
+@end
---
+++ EasyAppCore/EasyAppCore/EasyAppCore.m
@@ -0,0 +1,13 @@
+//
+//  EasyAppCore.m
+//  EasyAppCore
+//
+//  Created by cdm on 15/5/1.
+//  Copyright (c) 2015年 MyIdeaWay. All rights reserved.
+//
+
+#import "EasyAppCore.h"
+
+@implementation EasyAppCore
+
+@end
